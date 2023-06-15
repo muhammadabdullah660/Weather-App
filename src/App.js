ab7d2fbb8df68d523973e1a040401bdbf82c776f
Muhammad Abdullah <mafivs51@gmail.com>
2023-06-15 10:44:31 +0500

    Cleaning and setting up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container-fluid App">
+        <div className="row">
+          <div className="col-md-12">
+            <img
+              src="https://img.icons8.com/fluency/240/weather-forecast.png"
+              alt="weather-forecast"
+              className="App-logo"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <p id="mainHeading">Weather Forecast</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
